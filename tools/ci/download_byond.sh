5fca8c967b8d757c55e541494a031c4335326a21
buffyuwu <buffyvrc@gmail.com>
2026-01-15 09:58:55 -0500

    Update download_byond.sh

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -2,4 +2,4 @@
 set -e
 source dependencies.sh
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl "https://thefinalnights.com/516.1663_byond.zip" -o C:/byond.zip -A "The-Final-Nights/2.0 Continuous Integration"
+curl "https://secure.byond.com/download/build/516/516.1658_byond.zip" -o C:/byond.zip -A "The-Final-Nights/2.0 Continuous Integration"
